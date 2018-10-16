678651f807af848090d46ae44268206b2c03e04b
Drew <czosea72@uwosh.edu>
2018-10-16 18:05:09 -0500

    Index Nav Work

--- site/index.php
+++ site/index.php
@@ -9,5 +9,35 @@
 		<title>Computer Science Club</title>
 	</head>
 	<body>
+        <!-- top stuff -->
+        <p>
+            This is always here
+        </p>
+        <div>
+            <a href="index.php?nav=home">Home</a>
+            <a href="index.php?nav=about">About Us</a>
+            <a href="index.php?nav=events">Events</a>
+            <a href="index.php?nav=account">Account</a>
+        </div>
+        <div>
+            <?php
+            if (!isset($_GET["nav"])) {
+                include "home.php";
+            }
+            else if($_GET["nav"] == "home") {
+                include "home.php";
+            }
+            else if ($_GET["nav"] == "about") {
+                include "../aboutUsPage/_aboutUs.php";
+            }
+            else if ($_GET["nav"] == "events") {
+                include "events.php";
+            }
+            else if ($_GET["nav"] == "account") {
+                include "account.php";
+            }
+            ?>
+        </div>
+        <!-- bottom stuff -->
     </body>
 </html>
