4efefd51e0f815e77b2b74c1064a33845e5cf546
Drew <czosea72@uwosh.edu>
2018-10-16 17:28:38 -0500

    Site folder and index shell

---
+++ site/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="author" content="The Group">
+		<meta name="description" content="CS Club">
+		<meta name="keywords" content="IT,Resume,Programming">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Computer Science Club</title>
+	</head>
+	<body>
+    </body>
+</html>
