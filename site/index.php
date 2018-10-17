13c5606883013a442f3bec93025d64933760a6a0
Drew <czosea72@uwosh.edu>
2018-10-17 10:07:18 -0500

    Index Changes for banner and logo, plus layout changes

--- site/index.php
+++ site/index.php
@@ -10,29 +10,35 @@
 		<title>Computer Science Club</title>
 	</head>
 	<body>
+		<div class="main">
         <?php
-        include "php/nav.php";
+    	include "php/nav.php";
 
-        if (!isset($_GET["nav"])) {
-            $N = getInclude("home");
-        } else {
+    	if (!isset($_GET["nav"])) {
+        	$N = getInclude("home");
+			$contentWindowClass = "content-window-home";
+    	} else {
             $N = getInclude($_GET["nav"]);
+			$contentWindowClass = "content-window-" . $_GET["nav"];
         }
 
-        ?>
-        <p class="first">
-            UW-Oshkosh Computer <br>
-             Science Club
-        </p>
-        <p class="second">
-            <a href="index.php?nav=home"><input name="Home" type="submit" value="Home"></a>
-            <a href="index.php?nav=about"><input name="about" type="submit" value="About Us"></a>
+    	?>
+		<div class="banner-container">
+			<img class="logo" src="images/CSIcon.png" width="125px">
+		</div>
+    	<div class="first">
+        	UW-Oshkosh Computer Science Club
+    	</div>
+        <div class="nav">
+        	<a href="index.php?nav=home"><input name="Home" type="submit" value="Home"></a>
+        	<a href="index.php?nav=about"><input name="about" type="submit" value="About Us"></a>
             <a href="index.php?nav=events"><input name="Events" type="submit" value="Events"></a>
-            <a href="index.php?nav=account"><input name="Account" type="submit" value="Account"></a>
-        </p>
-        <div>
-            <?php include $N ?>
-        </div>
+        	<a href="index.php?nav=account"><input name="Account" type="submit" value="Account"></a>
+    	</div>
+    	<div class=<?= $contentWindowClass ?>>
+        	<?php include $N ?>
+		</div>
         <!-- bottom stuff -->
+	</div>
     </body>
 </html>
