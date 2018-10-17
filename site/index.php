2214a08149d8bb893a9fe440260f3ee0723dc82f
Drew <czosea72@uwosh.edu>
2018-10-17 18:38:27 -0500

    Refactoring Banner and Logo

--- site/index.php
+++ site/index.php
@@ -10,8 +10,7 @@
 		<title>Computer Science Club</title>
 	</head>
 	<body>
-		<div class="main">
-        <?php
+		<?php
     	include "php/nav.php";
 
     	if (!isset($_GET["nav"])) {
@@ -23,22 +22,32 @@
         }
 
     	?>
-		<div class="banner-container">
-			<img class="logo" src="images/CSIcon.png" width="125px">
-		</div>
-    	<div class="first">
-        	UW-Oshkosh Computer Science Club
-    	</div>
-        <div class="nav">
-        	<a href="index.php?nav=home"><input name="Home" type="submit" value="Home"></a>
-        	<a href="index.php?nav=about"><input name="about" type="submit" value="About Us"></a>
-            <a href="index.php?nav=events"><input name="Events" type="submit" value="Events"></a>
-        	<a href="index.php?nav=account"><input name="Account" type="submit" value="Account"></a>
-    	</div>
-    	<div class=<?= $contentWindowClass ?>>
-        	<?php include $N ?>
-		</div>
+		<div class="main">
+			<div class="main-top">
+				<div class="logo-wrapper">
+					<div class="logo-float">
+						<a href="index.php">
+							<img src="images/CSIcon.png" width="125px">
+						</a>
+					</div>
+				</div>
+				<div class="banner-container">
+					<img src="images/bannerc.jpg">
+				</div>
+				<div class="first">
+		        	UW-Oshkosh Computer Science Club
+		    	</div>
+				<div class="nav">
+		        	<a href="index.php?nav=home"><input name="Home" type="submit" value="Home"></a>
+		        	<a href="index.php?nav=about"><input name="about" type="submit" value="About Us"></a>
+		            <a href="index.php?nav=events"><input name="Events" type="submit" value="Events"></a>
+		        	<a href="index.php?nav=account"><input name="Account" type="submit" value="Account"></a>
+		    	</div>
+			</div>
+    		<div class=<?= $contentWindowClass ?>>
+        		<?php include $N ?>
+			</div>
         <!-- bottom stuff -->
-	</div>
+		</div>
     </body>
 </html>
