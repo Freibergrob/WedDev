f37e4d7ebf0c328565a60da8586183fafdf361f4
Drew <czosea72@uwosh.edu>
2018-10-20 13:51:31 -0500

    Adding content list to page

---
+++ site/homePage/_home.php
@@ -0,0 +1,8 @@
+<ul>
+    <li>Where we meet</li>
+    <li>Times</li>
+    <li>Learn more about us (with link)</li>
+    <li>Sales pitch (why should you join)</li>
+    <li>Link to signup</li>
+    <li>Next upcoming event</li>
+</ul>
