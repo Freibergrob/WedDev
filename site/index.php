8a95b2a7e45719256446b5dc209a35cc06b6e07c
Drew <czosea72@uwosh.edu>
2018-11-09 06:03:05 -0600

    Fixed a link and added the sitemap

--- site/index.php
+++ site/index.php
@@ -24,7 +24,7 @@
 				    <h3>Times</h3>
 				    <p>We meet every Thursday, excluding holidays, at 5:00 p.m. (Times may vary)</p>
 				    <h3>Learn more about us</h3>
-				    <p>Dying to know more about us? You can find more about us <a href="index.php?nav=about">here</a>!</p>
+				    <p>Dying to know more about us? You can find more about us <a href="<?=$_PATHS['link']?>aboutUsPage">here</a>!</p>
 				    <h3>Sales pitch </h3>
 				    <p>Joining UW Oshkosh's Computer Science Club will give you so much, for just simply signing up.
 				     You will meet people that are in your field, get hands on experience with different technology, solve a variety of programming problems
