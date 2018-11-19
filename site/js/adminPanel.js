79320e99ad56df864188c31ce3789a159a378087
Drew <czosea72@uwosh.edu>
2018-11-19 10:10:08 -0600

    Added features to TinyMCE

--- site/js/adminPanel.js
+++ site/js/adminPanel.js
@@ -2,9 +2,10 @@ var imageurl = window.location.href.split("site")[0] + "site/images/";
 
 tinymce.init({
     selector: ".save",
-    plugins: "lists advlist table autolink save anchor preview image advcode link",
-    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | numlist bullist | link | table | image | anchor | code preview save",
+    plugins: "lists advlist table autolink save anchor preview image advcode link textcolor",
+    toolbar: "undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | numlist bullist | link | table | image | anchor | code preview save",
     width: 800,
     height: 300,
-    image_prepend_url: imageurl
+    image_prepend_url: imageurl,
+    default_link_target: "_blank"
 });
