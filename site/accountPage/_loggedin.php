751b80b11d717a5b42a03b1eede71f161df110df
Drew <czosea72@uwosh.edu>
2018-12-06 16:38:25 -0600

    even more stuff

--- site/accountPage/_loggedin.php
+++ site/accountPage/_loggedin.php
@@ -22,22 +22,22 @@
 <p>
 <form>
     <label for="first">First Name</label>
-    <input type="input" id="first" class="first" placeholder="First Name">
+    <input type="input" id="first" name="first" class="first" placeholder="First Name">
     <label for="last">Last Name</label>
-    <input type="input" id="last" class="last" placeholder="Last Name">
+    <input type="input" id="last" name="last" class="last" placeholder="Last Name">
     <label for="nick">Nickname</label>
-    <input type="input" id="nick" class="nick" placeholder="Nickname">
+    <input type="input" id="nick" name="nick" class="nick" placeholder="Nickname">
     <label for="year">Graduation Year</label>
-    <select id="year" name="grad-year">
-    <option value="2018">2018</option>
-    <option value="2019">2019</option>
-    <option value="2020">2020</option>
-    <option value="2021">2021</option>
-    <option value="2022">2022</option>
-    <option value="2023">2023</option>
-    <option value="2024">2024</option>
-    <option value="2025">2025</option>
-    <option value="2026">2026</option>
+    <select id="year" name="gradyear">
+        <option value="2018">2018</option>
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+        <option value="2023">2023</option>
+        <option value="2024">2024</option>
+        <option value="2025">2025</option>
+        <option value="2026">2026</option>
     </select>
     <label for="fall">Fall</label>
     <input type="radio" id="fall" name="season" value="fall">
@@ -45,9 +45,7 @@
     <input type="radio" id="spring" name="season" value="spring">
     <label for="email">Email</label>
     <input type="input" id="email" name="email" placeholder="Email">
-    <label for="bio">Bio</label>
-    <textarea rows="4" cols="63">
-    Tell us about yourself...
-    </textarea>
+    <label for="bio">Biography</label>
+    <textarea rows="4" cols="63" name="aboutself" placeholder="Tell us about yourself..."></textarea>
 </form>
 </p>
