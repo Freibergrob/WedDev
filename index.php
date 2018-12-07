150e407020aa91394b058539c069ee61b48a6b85
freibergrob <freibergrob@gmail.com>
2018-12-07 14:46:13 -0600

    fixed styling on login.php and forms.css

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<?php require_once "../config.php";
+		      require_once $_PATHS['_meta.php']; ?>
+		<link rel="stylesheet" href="<?=$_PATHS['forms.css']?>">
+		<title>Computer Science Club</title>
+	</head>
+<body>
+<?php
+    session_start();
+    $loginerror = "";
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        include $_PATHS['loginProcess.php'];
+    }
+
+    if(isset($_SESSION["loggedin"]) && isset($_SESSION["role"]) && $_SESSION["loggedin"] === true && $_SESSION["role"] == 3) {
+        header("Location: " . "admin.php");
+        die();
+    }
+    else if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("Location: " . $_PATHS["link"]) ;
+    }
+    else if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $loginerror = "Access Denied!";
+    }
+ ?>
+        <form action="index.php" method="post">
+            <div class="wrapper">
+                <h2>Admin Login</h2>
+                <p class="error"><strong><?=$loginerror ?></strong></p>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                <div class="button">
+
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </div>
+            </div>
+        </form>
+        <?php include $_PATHS['_footer.php']; ?>
