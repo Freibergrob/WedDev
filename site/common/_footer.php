0fd317ab65897ba03fcf85bb6ddefe275e986a09
freibe96 <44214465+freibe96@users.noreply.github.com>
2018-12-06 19:01:06 -0600

    validate me

--- site/common/_footer.php
+++ site/common/_footer.php
@@ -4,3 +4,6 @@
     Nothing on the site should be construed in any way as being officially connected with
     or representative of UWO Computer Science Club.
 </div>
+<div class="validate">
+    Validate <a href="http://validator.w3.org/check/referer">me</a>!
+</div>
